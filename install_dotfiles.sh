b928b22848292a55dbee1911ab3277d0c0a7bc9c
Alihusein Kuwajerwala <aliqk1603@gmail.com>
2019-12-01 21:46:30 -0500

    added vs code

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,7 +7,15 @@ do
   echo "The Color is: ${COLOR}"
 done
 
+# bashrc stuff
 rm ~/.bashrc
 ln -sv ~/Projects/dotfiles/.bashrc ~
 
+# vscode stuff
+# settings
+rm ~/.config/Code/User/settings.json
+ln -sv ~/Projects/dotfiles/Code/User/settings.json ~/.config/Code/User/
 
+# shortcuts
+rm ~/.config/Code/User/keybindings.json
+ln -sv ~/Projects/dotfiles/Code/User/keybindings.json ~/.config/Code/User/
