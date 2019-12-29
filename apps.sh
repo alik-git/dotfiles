73c4aa0d6e3de9974595ced9a74709b03ad1acfb
Ali K <aliqk1603@gmail.com>
2019-12-29 16:14:47 -0500

    added gnome tweaks and removed steam stuff

--- apps.sh
+++ apps.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
+
+# installing apps 
 apt-get update  # To get the latest package lists
 apt-get install vim -y
 apt install build-essential -y
 apt-get install gimp -y
 snap install vlc -y
+apt install gnome-tweaks -y
+apt install adwaita-icon-theme-full
 apt-get install spotify-client -y
 apt install neofetch -y
 -H pip install thefuck -y
 apt install qbittorrent -y
 snap install emacs -y
-add-apt-repository multiverse
-apt install steam -y
-add-apt-repository ppa:mdeslaur/steamos
-apt-get update
-apt-get install steamos-xpad-dkms linux-headers-generic -y
-apt install sysfsutils
-echo "/module/bluetooth/parameters/disable_ertm=1" >> /etc/sysfs.conf
+
+# some settings changes 
 
 
+
+
+# VSCode stuff 
 apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
