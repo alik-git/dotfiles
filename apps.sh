8210487402724991e0e7cb737209babf286be01a
Ali K <aliqk1603@gmail.com>
2019-12-28 21:38:44 -0500

    added an apps installer script

---
+++ apps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt-get update  # To get the latest package lists
+apt-get install vim -y
+apt install build-essential -y
+apt-get install gimp -y
+snap install vlc -y
+apt-get install spotify-client -y
+
+apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt update
+apt install code -y
+#etc.
