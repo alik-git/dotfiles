b30b7eea0dc7fe8d4194d0a2ac181c8e18193989
Ali K <aliqk1603@gmail.com>
2019-12-28 21:42:52 -0500

    added neofetch

--- apps.sh
+++ apps.sh
@@ -5,10 +5,14 @@ apt install build-essential -y
 apt-get install gimp -y
 snap install vlc -y
 apt-get install spotify-client -y
+apt install neofetch -y
+
 
 apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt update
 apt install code -y
+
+
 #etc.
