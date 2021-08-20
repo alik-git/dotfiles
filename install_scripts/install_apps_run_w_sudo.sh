137ef81a470794bbf40099fad796ffe63454a823
Ali K <ali.kuwajerwala@mail.utoronto.ca>
2021-08-20 18:20:44 -0400

    added snap, moved rest down cause snap needs restart

--- install_scripts/install_apps_run_w_sudo.sh
+++ install_scripts/install_apps_run_w_sudo.sh
@@ -13,29 +13,21 @@ apt install qbittorrent -y
 apt-get install gparted -y
 apt install python3-dev python3-pip python3-setuptools -y
 apt-get install xclip -y
+apt install snapd -y
 
-pip3 install thefuck
-apt install thefuck -y
-
-# snap packages
-snap install vlc -y
-snap install emacs -y
-snap install slack --classic -y
-snap install --classic code -y 
-snap install discord -y # sometimes doesn't update properly
+# commented out for now
+# pip3 install thefuck
+# apt install thefuck -y
 
 # website stuff
 apt install ruby -y
 apt-get install ruby-dev -y
 apt install ruby-bundler -y
 
-# older stuff
+# snap packages
+snap install vlc
+snap install emacs
+snap install slack --classic
+snap install --classic code
+snap install discord # sometimes doesn't update properly
 
-# apt install software-properties-common apt-transport-https wget -y
-# wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
-# add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-# apt update
-# apt install code -y
-
-
-#etc.
