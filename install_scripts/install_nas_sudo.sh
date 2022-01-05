3e1dc4552d62954cfb9655abcec437ec7e7c9133
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-01-05 15:16:56 -0500

    added better instructions

--- install_scripts/install_nas_sudo.sh
+++ install_scripts/install_nas_sudo.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Make sure to put the your nas creds in the nas_creds file !!!
-# Make sure to set your local user's uid and gid in the personal_config/nas_stuff/mnt-MyStuff.mount file !!
+# Step 1: Actually connect to the NAS manually first. It should be visible on tank.local
+# Step 2: Make sure to create a nas_creds file using the nas_creds_examle file
+
+# Maybe needed: Make sure to set your local user's uid and gid in the personal_config/nas_stuff/mnt-MyStuff.mount file !!
 set -x
 
 mkdir -p /mnt/MyStuff
