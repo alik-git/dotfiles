9d4cb080282097a4ff2a2ac9f0ecb928e2d102b2
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-01-05 15:04:17 -0500

    added stuff

--- install_scripts/grub_pop_setup_sudo.sh
+++ install_scripts/grub_pop_setup_sudo.sh
@@ -4,4 +4,4 @@ apt install grub-efi grub2-common grub-customizer -y
 
 grub-install
 
-cp /boot/grub/x86_64-efi/grub.efi /boot/efi/EFI/pop/grubx64.efi
+# cp /boot/grub/x86_64-efi/grub.efi /boot/efi/EFI/pop/grubx64.efi
