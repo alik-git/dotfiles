fffe342c2bc354522a5d797154b11edbe18bcd03
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-01-05 15:32:12 -0500

    updates pia download

--- install_scripts/pia_vpn_setup.sh
+++ install_scripts/pia_vpn_setup.sh
@@ -3,8 +3,8 @@ set -x
 
 cd /tmp
 
-curl -Ok https://installers.privateinternetaccess.com/download/pia-linux-3.0-06692.run
+curl -Ok https://installers.privateinternetaccess.com/download/pia-linux-3.2-06857.run
 
-chmod +x pia-linux-3.0-06692.run 
+chmod +x pia-linux-3.2-06857.run 
 
-./pia-linux-3.0-06692.run 
+./pia-linux-3.2-06857.run 
