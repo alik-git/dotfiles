c20c5f5926cb6d10246c251256417d8b15197c6a
Ali K <alihusein.kuwajerwala@umontreal.ca>
2022-12-31 00:59:04 -0500

    added files for go

---
+++ install_scripts/go_w_sudo.sh
@@ -0,0 +1,266 @@
+# $ go get -d github.com/sylabs/singularity
+
+
+
+# $ export VERSION=v3.0.3 && \
+#     cd $GOPATH/src/github.com/sylabs/singularity && \
+#     git fetch && \
+#     git checkout $VERSION 
+
+# export VERSION=3.0.3 &&  \
+#     mkdir -p $GOPATH/src/github.com/sylabs && \
+#     cd $GOPATH/src/github.com/sylabs && \
+#     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+#     tar -xzf singularity-${VERSION}.tar.gz && \
+#     cd ./singularity && \
+#     ./mconfig
+
+
+# ./mconfig && \
+#     make -C ./builddir && \
+#     sudo make -C ./builddir install
+
+
+#      552  ssh -Y alik@narval.computecanada.ca
+#   553  ssh alik@narval.computecanada.ca
+#   554  ca jukebox 
+#   555  python jukebox/sample.py --model=1b_lyrics --name=sample_1b --levels=3 --sample_length_in_seconds=5 --total_sample_length_in_seconds=180 --sr=44100 --n_samples=16 --hop_fraction=0.5,0.5,0.125
+#   556  python jukebox/sample.py --model=1b_lyrics --name=sample_1b --levels=3 --sample_length_in_seconds=20 --total_sample_length_in_seconds=180 --sr=44100 --n_samples=16 --hop_fraction=0.5,0.5,0.125
+#   557  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   558  /home/kuwajerw/anaconda3/envs/gdf/bin/python /home/kuwajerw/repos/gradslam-foundation/examples/clip_feature_fusion_icl.py
+#   559  cd examples/
+#   560  ls -la
+#   561  /home/kuwajerw/anaconda3/envs/gdf/bin/python /home/kuwajerw/repos/gradslam-foundation/examples/clip_feature_fusion_icl.py
+#   562  ca gdf
+#   563  hs
+#   564  hs python
+#   565  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   566  pop
+#   567  conda remove --name gdf --all
+#   568  conda info --envs
+#   569  cd ..
+#   570  ebook-convert Windell\ Oskay_\ Eric\ Schlaepfer\ -\ Open\ Circuits_\ The\ Inner\ Beauty\ of\ Electronic\ Components-No\ Starch\ Press\ \(2022\).epub out.pdf
+#   571  ssh alihusein.kuwajerwala@login.server.mila.quebec -p 2222
+#   572  tree
+#   573  tree -d
+#   574  cd ..
+#   575  tree -d
+#   576  cd data/
+#   577  tree -d
+#   578  pwd
+#   579  ssh alik@uberduck.duckietown.local -J kuwajerwa@arcade.iro.umontreal.ca
+#   580  ssh alik@uberduck.duckietown.local -J kuwajera@arcade.iro.umontreal.ca
+#   581  sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+#   582  which ebook-convert
+#   583  nvtop
+#   584  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   585  tree
+#   586  tree -d
+#   587  cd ..
+#   588  tree
+#   589  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   590  cd examples/
+#   591  python extract_dino_features_icl.py
+#   592  python clip_feature_fusion_icl.py
+#   593  python extract_dino_features_icl.py
+#   594  python visualize_dino_feature_self_similarity.py
+#   595  q
+#   596  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   597  /home/kuwajerw/anaconda3/bin/conda install --name gdf pylint -y
+#   598  /home/kuwajerw/anaconda3/bin/conda install --name gdf black -y
+#   599  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   600  ca gdf
+#   601  python setup.py build develop
+#   602  ca gdf
+#   603  python setup.py build develop
+#   604  conda create -n gdf python=3.10.8
+#   605  ca gdf
+#   606  conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+#   607  python setup.py build develop
+#   608  pip install open3d
+#   609  pip install fast-pytorch-kmeans
+#   610  cd examples
+#   611  python extract_lseg_features_icl.py
+#   612  pip install git+https://github.com/openai/CLIP.git
+#   613  python extract_lseg_features_icl.py
+#   614  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   615  pip install "clip @ git+https://github.com/openai/CLIP.git"
+#   616  pip install "lavis @ git+https://github.com/salesforce/LAVIS.git"
+#   617  pip install clip-text-decoder
+#   618  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   619  pip install "lavis @ git+https://github.com/salesforce/LAVIS.git"
+#   620  pip install clip-text-decoder
+#   621  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   622  cd examples/
+#   623  python visualize_fused_feature_self_similarity_icl.py
+#   624  source /home/kuwajerw/anaconda3/bin/activate gdf2
+#   625  python
+#   626  source /home/kuwajerw/anaconda3/bin/activate gdf2
+#   627  source /home/kuwajerw/anaconda3/bin/activate gdf2
+#   628  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   629  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   630  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   631  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   632  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   633  /home/kuwajerw/anaconda3/envs/gdf/bin/python /home/kuwajerw/anaconda3/envs/gdf/lib/python3.10/site-packages/clip_text_decoder/model.py
+#   634  /home/kuwajerw/anaconda3/envs/gdf/bin/python /home/kuwajerw/repos/gradslam-foundation/coco_Test.py
+#   635  pip install pycocotools
+#   636  pip install skimage
+#   637  pip install scikit-image
+#   638  /home/kuwajerw/anaconda3/envs/gdf/bin/python /home/kuwajerw/repos/gradslam-foundation/coco_Test.py
+#   639  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   640  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   641  repos
+#   642  ca gdf
+#   643  git clone https://github.com/salesforce/LAVIS.git
+#   644  cd LAVIS/
+#   645  pip install .
+#   646  conda create -n gdf2
+#   647  conda create -n gdf2 python=3.8
+#   648  ca gdf2
+#   649  pip install pip install "lavis @ git+https://github.com/salesforce/LAVIS.git"
+#   650  pip install "lavis @ git+https://github.com/salesforce/LAVIS.git"
+#   651  conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+#   652  cd ..
+#   653  cd gradslam-foundation
+#   654  python setup.py build develop
+#   655  cd ..
+#   656  cd lseg-minimal/
+#   657  https://www.youtube.com/watch?v=ZY63ZSUdywY
+#   658  python setup.py build develop
+#   659  pip install fast-pytorch-kmeans
+#   660  pip install git+https://github.com/openai/CLIP.git
+#   661  pip install clip-text-decoder
+#   662  cd ~/data
+#   663  tree -d
+#   664  nvtop
+#   665  /home/kuwajerw/anaconda3/envs/gdf/bin/python /home/kuwajerw/repos/gradslam-foundation/examples/closed_vocab_classification_icl_coco.py
+#   666  git config pull.rebase false
+#   667  sudo apt-get install openssh-server
+#   668  sudo systemctl enable ssh --now
+#   669  sudo systemctl start ssh
+#   670  sudo systemctl status ssh
+#   671  sudo ufw allow ssh
+#   672  ip a
+#   673  nvtop
+#   674  source /home/kuwajerw/anaconda3/bin/activate cf
+#   675  /home/kuwajerw/anaconda3/envs/cf/bin/python /home/kuwajerw/repos/clip-fields/demo/parse_rgbd.py
+#   676  source /home/kuwajerw/anaconda3/bin/activate cf
+#   677  pip install python-opencv
+#   678  pip install opencv-python
+#   679  python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+#   680  source /home/kuwajerw/anaconda3/bin/activate cf
+#   681  pip install -r requirements.txt 
+#   682  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   683  ssh alihusein.kuwajerwala@login.server.mila.quebec -p 2222
+#   684  nvotp
+#   685  nvtop
+#   686  /home/kuwajerw/anaconda3/bin/conda install --name gdf autopep8 -y
+#   687  sudo swapoff -a
+#   688  sudo dd if=/dev/zero of=/swapfile bs=20G count=8
+#   689  sudo dd if=/dev/zero of=/swapfile bs=10G count=8
+#   690  ca cf
+#   691  pip install fairscale
+#   692  pip3 install transformers==4.15.0 timm==0.4.12 fairscale==0.4.4
+#   693  hs ssh
+#   694  ssh alihusein.kuwajerwala@login.server.mila.quebec -p 2222
+#   695  nvtop
+#   696  hs ssh
+#   697  ssh alihusein.kuwajerwala@login.server.mila.quebec -p 2222
+#   698  sudo add-apt-repository ppa:alessandro-strada/ppa
+#   699  sudo apt install google-drive-ocamlfuse
+#   700  mkdir ~/Google-drive
+#   701  ls
+#   702  google-drive-ocamlfuse ~/Google-drive
+#   703  cd Google-drive/
+#   704  ls
+#   705  ls 
+#   706  ls
+#   707  swapoff -a
+#   708  sudo swapoff -a
+#   709  fallocate -l 10G /swapfile
+#   710  sudo fallocate -l 10G /swapfile
+#   711  sudo fallocate -l 100G /swapfile
+#   712  chmod 600 /swapfile
+#   713  sudo chmod 600 /swapfile
+#   714  sudo mkswap /swapfile
+#   715  swapon /swapfile
+#   716  sudo swapon /swapfile
+#   717  free -m
+#   718  nvtop
+#   719  sudo swapoff -a
+#   720  sudo fallocate -l 400G /swapfile
+#   721  hs chm
+#   722  sudo chmod 600 /swapfile
+#   723  sudo mkswap /swapfile
+#   724  swapon /swapfile
+#   725  sudo swapon /swapfile
+#   726  ssh alik@uberduck.duckietown.local -J
+#   727  ssh alik@uberduck.duckietown.local --J
+#   728  ssh alik@uberduck.duckietown.local -J kuwajera@arcade.iro.umontreal.ca
+#   729  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   730  go version
+#   731  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   732  go version
+#   733  pop
+#   734  go version
+#   735  source /home/kuwajerw/anaconda3/bin/activate gdf
+#   736  go version
+#   737  pop
+#   738  cat bashrc
+#   739  cat ~/.bashrc
+#   740  go get -u github.com/golang/dep/cmd/dep
+#   741  go get -d github.com/sylabs/singularity
+#   742  export VERSION=v3.0.3 &&     cd $GOPATH/src/github.com/sylabs/singularity &&     git fetch &&     git checkout $VERSION 
+#   743  export VERSION=3.0.3 &&      mkdir -p $GOPATH/src/github.com/sylabs &&     cd $GOPATH/src/github.com/sylabs &&     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz &&     tar -xzf singularity-${VERSION}.tar.gz &&     cd ./singularity &&     ./mconfig
+#   744  cd -
+#   745  ./mconfig &&     make -C ./builddir &&     sudo make -C ./builddir install
+#   746  singularity --version
+#   747  hs
+#   748  history
+
+
+#     729  sudo apt-get update && sudo apt-get install -y     build-essential     libssl-dev     uuid-dev     libgpgme11-dev     squashfs-tools     libseccomp-dev     wget     pkg-config     git
+#   730  sudo apt-get update
+#   731  sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev     squashfs-tools libseccomp-dev wget pkg-config git cryptsetup debootstrap
+#   732  cd Downloads/
+#   733  wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+#   734  sudo tar --directory=/usr/local -xzvf go1.13.linux-amd64.tar.gz
+#   735  export PATH=/usr/local/go/bin:$PATH
+#   736  go version
+#   737  pop
+#   738  history
+
+
+
+set -x
+
+cd /tmp
+
+apt-get update
+
+apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev     squashfs-tools libseccomp-dev wget pkg-config git cryptsetup debootstrap
+
+
+
+wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+
+tar --directory=/usr/local -xzvf go1.13.linux-amd64.tar.gz
+
+echo "Remember to add this path to the bashrc!!!"
+echo "export PATH=/usr/local/go/bin:\$PATH"
+echo "Also add the path: export GOPATH=\${HOME}/go "
+
+# ### Add to Path ###
+
+echo "so basically add this"
+echo " export GOPATH=\${HOME}/go && export PATH=\$PATH:/usr/local/go/bin"
+echo "there you go"
+
+
+echo "!!!"
+
+
+echo "Also, confirm that this script ran correctly by checking the output of the comand: go version"
+
+# go version
