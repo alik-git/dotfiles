369ecb020825335da57f74f0601a1b409fab81f8
alik-git <akuwajerwala@minervahumanoids.com>
2026-03-21 12:11:18 -0400

    refactor: move bash aliases into separate file

---
+++ dot_config/bash/aliases.sh
@@ -0,0 +1,12 @@
+# Shell aliases sourced from ~/.bashrc.
+
+# Shpool convenience aliases.
+alias spp='shpool'
+alias spa='shpool attach'
+alias spl='shpool list'
+
+# Reload the current interactive shell config.
+alias pop='source ~/.bashrc'
+
+# Conda environment activation shortcut.
+alias ca='conda activate'
