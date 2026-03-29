6382f5a07807d9c0a4e4bafe28bcff70c5fddc39
alik-git <alihkwala@gmail.com>
2026-03-29 02:00:06 -0400

    fix: remove stale white_xps conda init

--- dot_config/bash/machine/white_xps.auto.sh
+++ dot_config/bash/machine/white_xps.auto.sh
@@ -3,18 +3,3 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/kuwajerw/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/kuwajerw/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/kuwajerw/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/kuwajerw/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
